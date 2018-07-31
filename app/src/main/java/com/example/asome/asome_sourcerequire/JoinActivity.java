34038a62feb576f57dff8c609530c9152c13fe9a
DESKTOP-914UAMH\Cho yoonyoung <yoon980208@naver.com>
2018-07-31 16:38:17 +0900

    0731윤영

--- app/src/main/java/com/example/asome/asome_sourcerequire/JoinActivity.java
+++ app/src/main/java/com/example/asome/asome_sourcerequire/JoinActivity.java
@@ -1,18 +1,47 @@
 package com.example.asome.asome_sourcerequire;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class JoinActivity extends AppCompatActivity {
 
+    EditText userName, userPwd, userDepartment, userPhone, userPosition, userCompany, userEmail;
+
+    public static final int DATABASE_VERSION =1;
+    public static final String DATABASE_NAME = "asome.db";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+        userName = findViewById(R.id.user_name);
+        userPwd = findViewById(R.id.user_pwd);
+        userDepartment = findViewById(R.id.user_phone);
+        userPhone = findViewById(R.id.user_phone);
+        userPosition = findViewById(R.id.user_position);
+        userCompany = findViewById(R.id.user_company);
+        userEmail = findViewById(R.id.user_email);
+
+
     }
+    String user_name = userName.getText().toString();
+    String user_pwd = userPwd.getText().toString();
+    String user_department = userDepartment.getText().toString();
+    String user_phone = userPhone.getText().toString();
+    String user_position = userPosition.getText().toString();
+    String user_email = userEmail.getText().toString();
+
+
+
 
     public void onJoinClicked(View view) {
         Intent intent = new Intent();
--- app/src/main/java/com/example/asome/asome_sourcerequire/RoleDetailDialogActivity.java
+++ app/src/main/java/com/example/asome/asome_sourcerequire/RoleDetailDialogActivity.java
@@ -7,11 +7,18 @@ import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class RoleDetailDialogActivity extends Activity {
     AutoCompleteTextView testTag;
-    TextView showTag;
+    TextView showTag,showStartDate,showEndDate;
+    DatePicker datePicker;
+    LinearLayout datePage;
+    String day, month, year;
+    Boolean startD=false;
+    Boolean endD=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +30,16 @@ public class RoleDetailDialogActivity extends Activity {
         ArrayAdapter<String> Tadapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_dropdown_item_1line, USER_TAG);
         testTag.setAdapter(Tadapter);
+        datePicker = findViewById(R.id.datePicker);
+        datePage = findViewById(R.id.datePage);
+
+        showStartDate = findViewById(R.id.resultStartDay);
+        showEndDate = findViewById(R.id.resultEndDay);
+
+        day =""+datePicker.getDayOfMonth();
+        month = ""+datePicker.getMonth();
+        year = ""+datePicker.getYear();
+
     }
     private static final String[] USER_TAG = new String[] {
             "@조윤영","@김연지","@황은선","@이경연"
@@ -31,9 +48,37 @@ public class RoleDetailDialogActivity extends Activity {
         finish();
     }
     public void userUploadClicked(View view) {
+        showTag.setVisibility(view.VISIBLE);
         String textTag = testTag.getText().toString();
-        String tag =textTag.substring(1,textTag.length());
+        String tag =textTag.substring(1,textTag.length())+":";
         showTag.setText(tag);
+
+
     }
 
+    public void setStartDateClicked(View view) {
+        datePage.setVisibility(view.VISIBLE);
+        startD=true;
+
+    }
+
+    public void setEndDateClicked(View view) {
+        datePage.setVisibility(view.VISIBLE);
+        endD=true;
+
+    }
+
+    public void onDateSelectClickd(View view) {
+        datePage.setVisibility(view.GONE);
+        if(startD==true){
+            showStartDate.setVisibility(view.VISIBLE);
+            showStartDate.setText(year+"-"+month+"-"+day);
+        }
+        if(endD==true) {
+            showEndDate.setVisibility(view.INVISIBLE);
+            showEndDate.setText(year+"-"+month+"-"+day);
+        }
+
+
+    }
 }
